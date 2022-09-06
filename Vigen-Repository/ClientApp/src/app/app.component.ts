ecc1d55fcd9d0ca51e1de6eb4b4c1e614412e392
santiago silva <santiagosil_h@hotmail.com>
2022-09-06 12:35:40 -0500

    Plantilla multi-Idioma

--- Vigen-Repository/ClientApp/src/app/app.component.ts
+++ Vigen-Repository/ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientApp';
+  constructor(public translate: TranslateService){
+    this.translate.addLangs(['es','en']);
+    this.translate.setDefaultLang('es');
+  }
+  changeLang(lang: string){
+    this.translate.use(lang);
+  }
 }
--- Vigen-Repository/ClientApp/src/app/app.module.ts
+++ Vigen-Repository/ClientApp/src/app/app.module.ts
@@ -4,13 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { NavComponent } from './nav/nav.component';
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/translate/','.json')
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
