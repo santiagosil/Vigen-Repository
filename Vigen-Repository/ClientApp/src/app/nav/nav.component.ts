926a89a46f4fa3be8d9c1409a3d6846a9a5fd043
santiago silva <santiagosil_h@hotmail.com>
2022-09-09 17:37:56 -0500

    Conexión del Backend a la base de datos y el Frontend al Backend

--- Vigen-Repository/ClientApp/src/app/nav/nav.component.ts
+++ Vigen-Repository/ClientApp/src/app/nav/nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../api/services';
+import { User } from '../api/models';
+import { empty } from 'rxjs';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -6,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+
+  constructor(private api: UserService) {
+    this.user={};
+    this.api.apiUserGet$Json({id:0}).subscribe(res=>{
+      this.user=res;
+    });
+  }
   ngOnInit(): void {
   }
 
