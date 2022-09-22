6ecac105ce91bac0a5b1ba9b52c73765c641869b
santiago silva <santiagosil_h@hotmail.com>
2022-09-22 12:39:14 -0500

    Restauracion Master

--- Vigen-Repository/ClientApp/src/app/app.module.ts
+++ Vigen-Repository/ClientApp/src/app/app.module.ts
@@ -8,36 +8,26 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { NavComponent } from './nav/nav.component';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-import { PanelEstadisticaComponent } from './panel-estadistica/panel-estadistica.component';
-=======
 import { SideComponent } from './side/side.component';
 import { PanelUserComponent } from './panel-user/panel-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { RecordOrgComponent } from './record-org/record-org.component';
 import { RecordUserComponent } from './record-user/record-user.component';
->>>>>>> af99e8c37138d5541dd65c1118670016bf2b304a
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/translate/', '.json')
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/translate/','.json')
 }
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-<<<<<<< HEAD
-    HomeComponent,
-    PanelEstadisticaComponent
-=======
     SideComponent,
     PanelUserComponent,
     EditUserComponent,
     RecordOrgComponent,
     RecordUserComponent
->>>>>>> af99e8c37138d5541dd65c1118670016bf2b304a
-  ],
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
