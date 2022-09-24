065f25cc701e05403a59c25d68a403544a61da8f
beller0 <brades8976@outlook.com>
2022-09-24 15:29:32 -0500

    creo la vista home y statistic dashboard

---
+++ Vigen-Repository/ClientApp/src/app/statistics-dashboard/statistics-dashboard.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StatisticsDashboardComponent } from './statistics-dashboard.component';
+
+describe('StatisticsDashboardComponent', () => {
+  let component: StatisticsDashboardComponent;
+  let fixture: ComponentFixture<StatisticsDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StatisticsDashboardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StatisticsDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
