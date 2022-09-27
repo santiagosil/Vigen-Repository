c5eb136c52ffa3c6822ecfb08c3c191061c1f9df
JEISSON DAVID INFANTE LEAL <jdinfante@ucundinamarca.edu.co>
2022-09-26 20:16:26 -0500

    La clase usa un correo dedicado para el envio y hace uso del cifrado StartTls

---
+++ Vigen-Repository/Email/Send.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Security;
+using MailKit.Net.Smtp;
+using System.Net;
+using System.Net.Mail;
+using MimeKit.Text;
+using MimeKit;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace Vigen_Repository.Email
+{
+    public class Send
+    {
+        public Send()
+        {
+
+        }
+        public string enviar(string correo, string numero)
+        {
+			try
+			{
+                
+                // create email message
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse("vigenservice@outlook.com"));
+                email.To.Add(MailboxAddress.Parse(correo));
+                email.Subject = "Test Email Subject";
+                email.Body = new TextPart(TextFormat.Plain) { Text = numero+"" };
+
+                // send email
+                SmtpClient smtp = new SmtpClient();
+                smtp.Connect("smtp.office365.com", 587, SecureSocketOptions.StartTls);
+                smtp.Authenticate("vigenservice@outlook.com", "vigen2022");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+                return "succesfull";
+            }
+			catch (Exception ex)
+			{
+
+				return ex.Message ;
+			}
+        }
+
+    }
+}
